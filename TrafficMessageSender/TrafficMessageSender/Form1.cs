05d57ce0e75f0e7b1d2d2f63629cd6be341e3c5d
MrJaeqx <lars.jaeqx@gmail.com>
2014-03-10 12:56:55 +0100

    Fix enzo

---
+++ TrafficMessageSender/TrafficMessageSender/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TrafficMessageSender
+{
+    public partial class MessageSenderForm : Form
+    {
+        // proxy om de TrafficMessageService te gebruiken
+        private TrafficMessageService.TrafficMessageClient myTrafficMessageProxy;
+        
+        public MessageSenderForm()
+        {
+            InitializeComponent();
+            myTrafficMessageProxy = new TrafficMessageService.TrafficMessageClient();
+
+        }
+
+        private void SendMessageBtn_Click(object sender, EventArgs e)
+        {
+            // VOEG ZELF TOE 
+            // code om de message in messageTextBox te versturen via web service TrafficMessageService als de user op de button klikt
+            // na afloop moet de TextBox weer leeg zijn
+
+        }
+    }
+}
---
+++ TrafficMessageService/Server/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using MessageService;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // zoek uit alle ips de default en gebruik deze voor de server
+            IPHostEntry IPhost;
+            string localIP = "";
+            IPhost = Dns.GetHostEntry(Dns.GetHostName());
+            localIP = IPhost.AddressList.FirstOrDefault(ip => ip.AddressFamily == AddressFamily.InterNetwork).ToString();
+
+            // creeer een host proces voor de TrafficMessageService
+            ServiceHost host = new ServiceHost(typeof(CTrafficMessage));
+
+            // creeer een zgn. end-point voor de service
+            Type contract = typeof(ITrafficMessage);
+            BasicHttpBinding binding = new BasicHttpBinding();
+            string baseAddress = "http://" + localIP + ":8000";
+            Uri address = new Uri(baseAddress + "/MessageService");
+            host.AddServiceEndpoint(contract, binding, address);
+
+            // creeer een zgn. mex endpoint om de wsdl van de service te hosten
+            host.Description.Behaviors.Add(new ServiceMetadataBehavior());
+            EndpointAddress endPointAddress = new EndpointAddress(baseAddress + "/MEX");
+            ServiceEndpoint mexEndpoint = new ServiceMetadataEndpoint(endPointAddress);
+            host.AddServiceEndpoint(mexEndpoint);
+
+            // start de service
+            host.Open();
+
+            // hou het proces in de lucht tot de gebruiker op enter drukt
+            Console.WriteLine("Service TrafficMessage successfully hosted at address: ");
+            Console.WriteLine("http://" + localIP + ":8000/MEX");
+            Console.WriteLine("\nPress <enter> to end the service ...");
+            Console.ReadLine();
+        }
+
+    }
+}
