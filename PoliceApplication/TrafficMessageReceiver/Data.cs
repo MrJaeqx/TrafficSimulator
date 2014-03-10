3b373700ab092307ef329e18726ae80f28ef911c
MrJaeqx <lars.jaeqx@gmail.com>
2014-03-10 14:39:09 +0100

    Data Class

--- PoliceApplication/TrafficMessageReceiver/Data.cs
+++ PoliceApplication/TrafficMessageReceiver/Data.cs
@@ -11,6 +11,11 @@ namespace TrafficMessageReceiver
         // proxy om de TrafficMessageService te gebruiken
         private TrafficMessageService.TrafficMessageClient myTrafficMessageProxy;
 
+        // lists met de data per type
+        private List<Accident> accidentList;
+        private List<RedLight> redLightList;
+        private List<Speeding> speedingList;
+
         public string ServerName { get; private set; }
 
         Data(string serverIP, string serverPort)
@@ -18,7 +23,40 @@ namespace TrafficMessageReceiver
             myTrafficMessageProxy = new TrafficMessageReceiver.TrafficMessageService.TrafficMessageClient();
             myTrafficMessageProxy.Endpoint.Address = new System.ServiceModel.EndpointAddress("http://" + serverIP + ":" + serverPort + "/MEX");
 
+            accidentList = new List<Accident>();
+            redLightList = new List<RedLight>();
+            speedingList = new List<Speeding>();
+
             ServerName = myTrafficMessageProxy.GetServerName();
+        }
+
+        public List<Accident> GetAccidents()
+        {
+            UpdateDataLists();
+            return accidentList;
+        }
+
+        public List<RedLight> GetRedLights()
+        {
+            UpdateDataLists();
+            return redLightList;
+        }
+
+        public List<Speeding> GetSpeedings()
+        {
+            UpdateDataLists();
+            return speedingList;
+        }
+
+        public void SaveXML()
+        {
+
+        }
+
+        private void UpdateDataLists()
+        {
+            string xml = myTrafficMessageProxy.RetrieveMessage();
+
         }
 
 
