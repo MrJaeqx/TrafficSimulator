78b2260e964da22243355cf19c0e62b5adf31b21
MrJaeqx <lars.jaeqx@gmail.com>
2014-03-10 15:26:50 +0100

    Comments enzo

--- PoliceApplication/TrafficMessageReceiver/Data.cs
+++ PoliceApplication/TrafficMessageReceiver/Data.cs
@@ -6,59 +6,94 @@ using System.Xml;
 
 namespace TrafficMessageReceiver
 {
+    /// <summary>
+    /// Deze class haalt alle data van de server.
+    /// </summary>
     class Data
     {
-        // proxy om de TrafficMessageService te gebruiken
+        /// <summary>Proxy om de TrafficMessageService te gebruiken.</summary>
         private TrafficMessageService.TrafficMessageClient myTrafficMessageProxy;
 
-        // lists met de data per type
+        /// <summary>Lists met de ongelukken</summary>
         private List<Accident> accidentList;
+        /// <summary>Lists met de rood licht overtredingen</summary>
         private List<RedLight> redLightList;
+        /// <summary>Lists met de snelheidsovertredingen</summary>
         private List<Speeding> speedingList;
 
+        /// <summary>Servernaam</summary>
         public string ServerName { get; private set; }
 
-        Data(string serverIP, string serverPort)
+        /// <summary>Constructor</summary>
+        /// <param name="serverIP">Een string die het ip van de server bevat</param>
+        /// <param name="serverPort">Een string die de poort van de server bevat</param>
+        public Data(string serverIP, string serverPort)
         {
+            // maak de connectie met de server
             myTrafficMessageProxy = new TrafficMessageReceiver.TrafficMessageService.TrafficMessageClient();
             myTrafficMessageProxy.Endpoint.Address = new System.ServiceModel.EndpointAddress("http://" + serverIP + ":" + serverPort + "/MEX");
 
+            // initaliseer de lists
             accidentList = new List<Accident>();
             redLightList = new List<RedLight>();
             speedingList = new List<Speeding>();
 
+            // set de servername property
             ServerName = myTrafficMessageProxy.GetServerName();
         }
 
+        /// <summary>Update de list en return de ongelukken</summary>
+        /// <returns>Returnt een List met Accident objecten</returns>
         public List<Accident> GetAccidents()
         {
             UpdateDataLists();
             return accidentList;
         }
 
+        /// <summary>Update de list en return de rood licht overtredingen</summary>
+        /// <returns>Returnt een List met RedLight objecten</returns>
         public List<RedLight> GetRedLights()
         {
             UpdateDataLists();
             return redLightList;
         }
 
+        /// <summary>Update de list en return de snelheidsovertredingen</summary>
+        /// <returns>Returnt een List met Speeding objecten</returns>
         public List<Speeding> GetSpeedings()
         {
             UpdateDataLists();
             return speedingList;
         }
 
-        public void SaveXML()
+        /// <summary>Sla de XML op in een bestand</summary>
+        /// <param name="filePath">Een string die het bestandspad bevat</param>
+        public void SaveXML(string filePath)
         {
 
         }
 
+        /// <summary>Haal de data van de server en update de lists</summary>
         private void UpdateDataLists()
         {
-            string xml = myTrafficMessageProxy.RetrieveMessage();
+            // haal de xml van de server
+            string XmlString = myTrafficMessageProxy.RetrieveMessage();
+            XmlDocument XmlData = new XmlDocument();
+            XmlData.LoadXml(XmlString);
 
+            // Haal alle rood lichten uit de xml
+            XmlNodeList RedLights = XmlData.GetElementsByTagName("redlights");
+
+            foreach (XmlNodeList RedLight in RedLights)
+            {
+                int carID = Convert.ToInt32(RedLight[0].InnerText);
+                int trafficlightID = Convert.ToInt32(RedLight[1].InnerText);
+
+                string[] timeComponents = RedLight[2].InnerText.Split();
+                DateTime time = new DateTime(Convert.ToInt32(timeComponents[0]), Convert.ToInt32(timeComponents[1]), Convert.ToInt32(timeComponents[2]));
+
+                redLightList.Add(new RedLight(carID, trafficlightID, time));
+            }
         }
-
-
     }
 }
