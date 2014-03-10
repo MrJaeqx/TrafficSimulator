aee4aeb567af64c9a1437fbe52f64ce22f88bd59
MrJaeqx <lars.jaeqx@gmail.com>
2014-03-10 14:13:52 +0100

    Police Data Class

---
+++ PoliceApplication/TrafficMessageReceiver/Data.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace TrafficMessageReceiver
+{
+    class Data
+    {
+        // proxy om de TrafficMessageService te gebruiken
+        private TrafficMessageService.TrafficMessageClient myTrafficMessageProxy;
+
+        public string ServerName { get; private set; }
+
+        Data(string serverIP, string serverPort)
+        {
+            myTrafficMessageProxy = new TrafficMessageReceiver.TrafficMessageService.TrafficMessageClient();
+            myTrafficMessageProxy.Endpoint.Address = new System.ServiceModel.EndpointAddress("http://" + serverIP + ":" + serverPort + "/MEX");
+
+            ServerName = myTrafficMessageProxy.GetServerName();
+        }
+
+
+    }
+}
