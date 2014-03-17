9e7c3a3b0ffe26f21ab2cc5606003c43e781b9a6
MrJaeqx <lars.jaeqx@gmail.com>
2014-03-17 15:19:42 +0100

    Begin Type 3

---
+++ TrafficSimulator/LogicControlType3.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TrafficSimulatorUi;
+
+namespace TrafficSimulator
+{
+    class LogicControlType3 : LogicControl
+    {
+        public override IntersectionControl Intersection { get; protected set; }
+
+        public override List<LaneId> Queue { get; protected set; }
+
+        public LogicControlType3(List<IntersectionControl> intersections)
+        {
+            foreach (IntersectionControl intersection in intersections)
+            {
+                if (intersection.IntersectionType == IntersectionType.TYPE_3)
+                {
+                    Intersection = intersection;
+                }
+            }
+        }
+
+        public override void MakeTurn()
+        {
+
+        }
+
+        public override void RemoveEndOFLaneRoadUser()
+        {
+
+        }
+
+        public override void HandleTrafficLight()
+        {
+
+        }
+
+        public override void HandleQueue()
+        {
+
+        }
+    }
+}
