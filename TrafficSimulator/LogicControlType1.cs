6e375f0b24a438866c9cd7f6f27dc038a3246c74
Wouter Habets <wouterhabets@gmail.com>
2014-03-17 17:06:55 +0100

    Type2: dingen

--- TrafficSimulator/LogicControlType1.cs
+++ TrafficSimulator/LogicControlType1.cs
@@ -10,9 +10,8 @@ namespace TrafficSimulator
 {
     public class LogicControlType1 : LogicControl
     {
-        public override IntersectionControl Intersection { get; protected set; }
-
         public override List<LaneId> Queue { get; protected set; }
+
         public LogicControlType1(List<IntersectionControl> intersections)
         {
             foreach (IntersectionControl intersection in intersections)
@@ -26,12 +25,49 @@ namespace TrafficSimulator
 
         public override void MakeTurn()
         {
-            throw new NotImplementedException();
+            foreach (RoadUser roadUser in Intersection.RoadUsers)
+            {
+                Point p = roadUser.Location;
+
+                if (roadUser.Direction == 270)
+                {
+
+                }
+                else if (roadUser.Direction == 180)
+                {
+
+                }
+                else if (roadUser.Direction == 90)
+                {
+
+                }
+                else if (roadUser.Direction == 0)
+                {
+
+                }
+            }
         }
 
         public override void RemoveEndOFLaneRoadUser()
         {
-            throw new NotImplementedException();
+            if (base.Intersection.RoadUsers.Count > 0)
+            {
+                foreach (RoadUser roadUser in Intersection.RoadUsers)
+                {
+                    Point p = roadUser.Location;
+
+                    if (p.X >= 400 || p.X <= -32)
+                    {
+                        Intersection.RemoveRoadUser(roadUser);
+                        break;
+                    }
+                    else if (p.Y >= 400 || p.Y <= -32)
+                    {
+                        Intersection.RemoveRoadUser(roadUser);
+                        break;
+                    }
+                }
+            }
         }
 
         public override void HandleTrafficLight()
