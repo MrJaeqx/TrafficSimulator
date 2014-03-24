a53e2ad302159fb7ec5260efca180c8383a61b52
corruptpony <tpj_ackermans@hotmail.com>
2014-03-24 16:45:05 +0100

    random car spawner class toegevoeged

---
+++ TrafficSimulator/RandomRoadUsers.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TrafficSimulatorUi;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace TrafficSimulator
+{
+    class RandomRoadUsers
+    {
+        public List<IntersectionControl> Intersections { get; private set; }
+
+        const int newRoadUserSpawnRate = 1000;
+
+        Timer timer;
+
+        public RandomRoadUsers(List<IntersectionControl> intersections)
+        {
+            Intersections = intersections;
+
+            timer = new Timer();
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Interval = newRoadUserSpawnRate;
+            timer.Start();
+        }
+
+        private void timer_Tick(Object myObject, EventArgs myEventArgs)
+        {
+            Random randomIntersection = new Random();
+            Random carType = new Random();
+            Random whereToSpawn = new Random();
+
+            IntersectionControl intersection = Intersections[randomIntersection.Next(0, 7)];
+
+            if (intersection.IntersectionType == IntersectionType.TYPE_1)
+            {
+
+            }
+            else if (intersection.IntersectionType == IntersectionType.TYPE_2)
+            {
+
+            }
+            else if (intersection.IntersectionType == IntersectionType.TYPE_3)
+            {
+
+            }
+            else if (intersection.IntersectionType == IntersectionType.TYPE_4)
+            {
+
+            }
+            else if (intersection.IntersectionType == IntersectionType.TYPE_5)
+            {
+
+            }
+            else if (intersection.IntersectionType == IntersectionType.TYPE_RAILWAY)
+            {
+
+            }
+        }
+    }
+}
