6d508d25003e27311cc08289c57fd5e2ef501ce3
Wouter Habets <wouterhabets@gmail.com>
2014-04-01 20:33:46 +0200

    MessageSender class

---
+++ TrafficSimulator/TrafficMessageSender.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TrafficSimulator
+{
+    public static class TrafficMessageSender
+    {
+
+        public static void SendAccident(int junctionID)
+        {
+            new TrafficMessageService.TrafficMessageClient().SendAccident(junctionID, DateTime.Now);
+        }
+
+        public static void SendRedLight(int carID, int trafficLightID)
+        {
+            new TrafficMessageService.TrafficMessageClient().SendRedLight(carID, trafficLightID, DateTime.Now);
+        }
+
+        public static void SendSpeeding(int carID, int carSpeed)
+        {
+            new TrafficMessageService.TrafficMessageClient().SendSpeeding(carID, carSpeed, DateTime.Now);
+        }
+    }
+}
